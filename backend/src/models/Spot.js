513619fa6034b4a3f0e57bb699ccb6ce20c4ebda
Vitor Bonet <vitorbonet@hotmail.com>
2019-10-29 23:48:24 -0300

    init

---
+++ backend/src/models/Spot.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const SpotSchema = new mongoose.Schema({
+    thumbnail: String,
+    company: String,
+    price: String,
+    techs: [String],
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, {
+    toJSON: {
+        virtuals: true,
+    }
+});
+
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `http://10.8.145.100:3333/files/${this.thumbnail}`
+})
+
+module.exports = mongoose.model('Spot', SpotSchema);
